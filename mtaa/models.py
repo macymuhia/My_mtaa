85ffa9990d69347a0b1c29594fc84743fd5d4f29
macymuhia <macymuhia@gmail.com>
2019-09-14 08:46:29 +0300

    Add model class for Userprofile

--- mtaa/models.py
+++ mtaa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    photo = models.ImageField(
+        upload_to="vibes/",
+        max_length=255,
+        null=True,
+        blank=True,
+        default="/static/img/default.png",
+    )
+    phone = models.CharField(max_length=20, blank=True, default="")
+    email_confirmed = models.BooleanField(default=False)
+    bio = models.TextField()
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.profile.save()
