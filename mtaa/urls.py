0099d3c26db1e1b80a2078f83233dfb7a50a1bd5
macymuhia <macymuhia@gmail.com>
2019-09-15 08:46:47 +0300

    Add path where the search results will be directed

--- mtaa/urls.py
+++ mtaa/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("signup", views.signup, name="signup"),
     path("login/profile/", views.profile, name="profile"),
     path("login/profile/edit/", views.edit_profile, name="edit_profile"),
+    path("search/", views.search_results, name="search_results"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
