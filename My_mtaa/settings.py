19c5e56d1ffdb127eae18900e004bc0becc4d651
macymuhia <macymuhia@gmail.com>
2019-09-16 10:54:57 +0300

    Add settings file with changes to enable deployment

--- My_mtaa/settings.py
+++ My_mtaa/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,13 +22,39 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+
+# Deployment
+MODE = config("MODE", default="dev")
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
+# development
+if config("MODE") == "dev":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": config("DB_NAME"),
+            "USER": config("DB_USER"),
+            "PASSWORD": config("DB_PASSWORD"),
+            "HOST": config("DB_HOST"),
+            "PORT": "",
+        }
+    }
+# production
+else:
+    DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "uza95-2_3za@09pd=b*7d@cid6_ni_etf72m&exr=bsphwt%*#"
+# SECRET_KEY = "uza95-2_3za@09pd=b*7d@cid6_ni_etf72m&exr=bsphwt%*#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "login/"
@@ -47,6 +76,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -80,14 +110,14 @@ WSGI_APPLICATION = "My_mtaa.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mtaa",
-        "USER": "macy",
-        "PASSWORD": "1234",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "mtaa",
+#         "USER": "macy",
+#         "PASSWORD": "1234",
+#     }
+# }
 
 
 # Password validation
@@ -123,6 +153,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
